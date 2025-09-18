b23f4265024edf372e91ea518bb856070a440e90
akkimahesh <maheshakki799@gmail.com>
2025-09-18 23:12:31 +0530

    initial commit

--- 01-mysql-installation.sh
+++ 01-mysql-installation.sh
@@ -7,9 +7,20 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then
 	echo "switch User"
+	exit 1
 else
 	echo "you're in Root User"
 fi
 
+echo "installing mysql server"
+
+dnf install mysql-server -y
+
+if [ $? -eq 0 ]
+then
+	echo "mysql server installed successfully"
+else
+	echo "mysql server installation failed"
+fi
 
 
