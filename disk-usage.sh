305d5ec88a6f2516b64ce36f5ad01a7db5312282
akkimahesh <maheshakki799@gmail.com>
2025-11-21 12:14:15 +0530

    initial commit

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-DISK_USAGE=$( df -hT | grep -vE 'tmp|File|fat' | awk '{print $6f}' | cut -d % -f1 )
+DISK_USAGE=$( df -hT | grep -vE 'tmp|File|fat')
 Treshold=10
-while IFS= read -r USAGE; do
+
+while IFS= read -r line; do
+
+    USAGE=$( echo "$line" | awk '{print $6f}' | cut -d '%' -f1)
+    partion=$( echo "$line" | awk '{print $1f}' )
+
     if [ "$USAGE" -ge "$Treshold" ]; then
-        echo "Warning: Disk usage is at ${USAGE}%"
+        echo "Warning: Partition $partion is at ${USAGE}% usage."
     fi
 done <<< "$DISK_USAGE"
