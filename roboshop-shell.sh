2f675751dfa086b88655d13c830d9ba9429c0e28
akkimahesh <maheshakki799@gmail.com>
2025-12-03 12:11:58 +0530

    initial commit

--- roboshop-shell.sh
+++ roboshop-shell.sh
@@ -1,24 +1,41 @@
+
+#!/bin/bash
+
 AMI=ami-095165dc733487595
-SG=sg-0e60afdae6e80a348
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq")
+SG=sg-0e60afdae6e80a348 
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
+DOMAIN_NAME="maheshakki.shop""
 
-for i in "${INSTANCES[@]}"; do
-    if [[ "$i" == "mongodb" || "$i" == "mysql" || "$i" == "shipping" ]]; then
-        INSTANCE_TYPE=t2.medium
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE=t2.micro
+        INSTANCE_TYPE="t2.micro"
     fi
-    echo "Instance: $i uses $INSTANCE_TYPE"
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
-
-# aws ec2 run-instances \
-# --image-id $AMI \
-# --instance-type $INSTANCE_TYPE \
-# --security-group-ids $SG \
-# --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-
-aws ec2 run-instances --image-id $AMI \
---instance-type $INSTANCE_TYPE \
---security-group-ids $SG \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
---query 'Instances[0].PrivateIpAddress' --output text
